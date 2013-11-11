ace5031742e86d7169b5ef497af1cbae0adf9900
Geoffrey Jones <jonesg5@rpi.edu>
2013-11-11 17:01:57 -0500

    Hide output in gaussian

--- archive/gaussian.m
+++ archive/gaussian.m
@@ -8,6 +8,6 @@ b = gausswin(window);
 
 [H, W] = freqz(b, 1, 10000);
 index = find(abs(H)<(0.70710678118654752440084436210485*H(1)),1,'first'); %find index of first value that is less than 0.5 in magnitude
-period3dB = (2*pi)/(60*srate*W(index))
+period3dB = (2*pi)/(60*srate*W(index));
 
 filtered = filtfilt(gausswin(window)/sum(gausswin(window)), 1, data);
